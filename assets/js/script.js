2605e98755ccd0c089c664ef300bb6d2a88c6381
Firman Farel Richardo <farelpasaribu04@gmail.com>
2025-10-01 17:10:25 +0700

    reconstruction

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript untuk mendapatkan tahun saat ini secara dinamis untuk footer
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// --- JavaScript untuk menu aktif saat scroll akan ditambahkan di sini ---
+// Bootstrap memiliki plugin ScrollSpy yang bisa kita gunakan untuk ini nanti!
